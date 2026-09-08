985f02df2ba196cef64882ee2bd252fb8fd228da
mromanowski832-ctrl <mromanowski832@gmail.com>
2026-09-08 11:59:41 +0200

    Add isolated test lifecycle

---
+++ src/test/java/pl/luzmen/qa/core/BaseTest.java
@@ -0,0 +1,35 @@
+package pl.luzmen.qa.core;
+
+import pl.luzmen.qa.driver.DriverFactory;
+import pl.luzmen.qa.driver.DriverManager;
+import pl.luzmen.qa.pages.LoginPage;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class BaseTest {
+
+    protected static final String STANDARD_USER = "standard_user";
+    protected static final String LOCKED_USER = "locked_out_user";
+    protected static final String PASSWORD = "secret_sauce";
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() {
+        DriverManager.setDriver(DriverFactory.createDriver());
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        DriverManager.quitDriver();
+    }
+
+    protected LoginPage loginPage() {
+        return new LoginPage(DriverManager.getDriver());
+    }
+
+    protected void loginAsStandardUser() {
+        loginPage()
+                .open()
+                .loginAs(STANDARD_USER, PASSWORD)
+                .assertLoaded();
+    }
+}
