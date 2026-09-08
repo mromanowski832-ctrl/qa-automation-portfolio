f90ef79275b96b83d743a1850025bedec618cdef
mromanowski832-ctrl <mromanowski832@gmail.com>
2026-09-08 12:05:45 +0200

    Register failure listener on all QA tests

--- src/test/java/pl/luzmen/qa/core/BaseTest.java
+++ src/test/java/pl/luzmen/qa/core/BaseTest.java
@@ -2,10 +2,13 @@ package pl.luzmen.qa.core;
 
 import pl.luzmen.qa.driver.DriverFactory;
 import pl.luzmen.qa.driver.DriverManager;
+import pl.luzmen.qa.listeners.TestListener;
 import pl.luzmen.qa.pages.LoginPage;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners(TestListener.class)
 public abstract class BaseTest {
 
     protected static final String STANDARD_USER = "standard_user";
